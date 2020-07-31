ee8b87e4d376f7b97e7a73a990c651a27f634a81
drewhsu86 <bowserhsu@gmail.com>
2020-07-31 13:35:38 -0400

    rudimentary sign up and sign in page in react, works with backend in local development

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
+
+import Signin from './components/users/Signin'
+import Signup from './components/users/Signup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" exact>
+        <h1>Root</h1>
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
     </div>
   );
 }
---
+++ client/src/components/users/Signup.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { signup } from '../../services/apiUsers'
+
+export default function Signin() {
+
+  const [username, setUsername] = useState(''); 
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState('');
+
+  console.log(username, password);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await signup({
+      username, password, email
+    });
+
+    console.log(response)
+  }
+
+  return (
+    <div>
+      
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        <button>Submit</button>
+
+      </form>
+
+    </div>
+  )
+}
