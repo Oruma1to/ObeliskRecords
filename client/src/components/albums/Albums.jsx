98aab9cb81826d46e2c573b49dbe20c3bc3fdf62
Juan Delgado <Delgado-juan@hotmail.com>
2020-08-03 20:06:59 -0400

    Here added Key for each child in the map section.The reason why is because it was giving me warnings. So i just added since each within the map has to have a unique Key . I gave the album id

--- client/src/components/albums/Albums.jsx
+++ client/src/components/albums/Albums.jsx
@@ -1,40 +1,41 @@
-import React, {useState, useEffect} from 'react'
-import { getAlbums } from '../../services/apiAlbums'
-import {Link} from 'react-router-dom'
-import './Albums.css'
+import React, { useState, useEffect } from 'react';
+import { getAlbums } from '../../services/apiAlbums';
+import { Link } from 'react-router-dom';
+import './Albums.css';
 
 export default function Albums() {
-
-  const [albums, setAlbums] = useState([])
+  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
-    pullAlbums()
-  }, [])
+    pullAlbums();
+  }, []);
 
   const pullAlbums = async () => {
     try {
       const response = await getAlbums();
-      console.log(response)
-      setAlbums(response)
+      console.log(response);
+      setAlbums(response);
     } catch (error) {
       console.log(error);
       this.setState({
-        status: error.message || 'Failed to load albums.'
-      })
+        status: error.message || 'Failed to load albums.',
+      });
     }
-  }
-    return (
-      <div>
-        {albums.map(album => {
-          return <div>
-            <Link className="album-container" to={`/albums/${album._id}`}>
-            <img className="album-cover" src={album.albumCover} />
-            <p className="album-title">{album.artistName}</p>
-            <p className="album-name">{album.albumName}</p>
-            <p className="album-year">{album.year}</p>
+  };
+  return (
+    <>
+      {albums.map(album => {
+        return (
+          <div key={album.id}>
+            <Link className='album-container' to={`/albums/${album._id}`}>
+              <img className='album-cover' src={album.albumCover} />
+              <p className='album-title'>{album.artistName}</p>
+              <p className='album-name'>{album.albumName}</p>
+              <p className='album-year'>{album.year}</p>
             </Link>
-            </div>
-        })}
-      </div>
-    )
+          </div>
+        );
+      })}
+    </>
+  );
 }
