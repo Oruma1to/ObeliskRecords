2444734607c6fa9cbf3ea1c851f7a8191eb83fa1
drewhsu86 <bowserhsu@gmail.com>
2020-08-05 17:57:52 -0400

    useEffect import corrected

--- client/src/components/albums/CRUD/EditAlbum.jsx
+++ client/src/components/albums/CRUD/EditAlbum.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useParams, useEffect } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { editAlbum, getOneAlbum } from '../../../services/apiAlbums'
 import './CRUD.css'
 
