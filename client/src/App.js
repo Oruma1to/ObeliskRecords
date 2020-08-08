249031591c5955d6a35f625ac6023f801717d014
Juan Delgado <Delgado-juan@hotmail.com>
2020-08-08 14:47:35 -0400

    Created the apiservices in the user file to getUser. I also create the MyProfile components and the css for it. Lastly create the route in App.js for MyProfile

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,78 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { verifyuser } from './services/apiUsers'
-import { logIn } from './actions'
+import { Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { verifyuser } from './services/apiUsers';
+import { logIn } from './actions';
 
-import Signin from './components/users/Signin'
-import Signup from './components/users/Signup'
-import Albums from './components/albums/Albums'
+import Signin from './components/users/Signin';
+import Signup from './components/users/Signup';
+import Albums from './components/albums/Albums';
 
 import Header from './components/shared/Header';
-import Home from './components/shared/Home'
+import Home from './components/shared/Home';
 import Album from './components/albums/Album';
-import CreateAlbum from './components/albums/CRUD/CreateAlbum'
-import EditAlbum from './components/albums/CRUD/EditAlbum'
+import CreateAlbum from './components/albums/CRUD/CreateAlbum';
+import EditAlbum from './components/albums/CRUD/EditAlbum';
+import MyProfile from './components/users/MyProfile';
 
-import ShoppingCart from './components/shoppingCart/ShoppingCart'
+import ShoppingCart from './components/shoppingCart/ShoppingCart';
 
 function App() {
   // const isLogged = useSelector(state => state.isLogged)
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  // on component did mount, try to log in and set the user 
+  // on component did mount, try to log in and set the user
 
   useEffect(() => {
-    initialize()
-  }, []
-  )
-
+    initialize();
+  }, []);
 
   const initialize = async () => {
     try {
-      const response = await verifyuser()
-      console.log(response)
-      dispatch(logIn(response))
+      const response = await verifyuser();
+      console.log(response);
+      dispatch(logIn(response));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      <div style={
-        {
-          height: '100px'
-        }
-      }></div>
-      <Route path="/" exact>
+      <div
+        style={{
+          height: '100px',
+        }}></div>
+      <Route path='/' exact>
         <Home />
         {/* <h1>IsLogged {isLogged ? "yes" : "no"}</h1>
         <button onClick={() => dispatch(logIn())}>+</button> */}
       </Route>
-      <Route path="/albums/:id">
+      <Route path='/albums/:id'>
         <Album />
       </Route>
-      <Route path="/signin">
+      <Route path='/signin'>
         <Signin />
       </Route>
-      <Route path="/signup">
+      <Route path='/signup'>
         <Signup />
       </Route>
-      <Route exact path="/albums">
+      <Route exact path='/albums'>
         <Albums />
       </Route>
-      <Route path="/createalbum">
+      <Route path='/createalbum'>
         <CreateAlbum />
       </Route>
-      <Route path="/editalbum/:id">
+      <Route path='/editalbum/:id'>
         <EditAlbum />
       </Route>
-      <Route exact path="/cart/">
+      <Route exact path='/cart/'>
         <ShoppingCart />
+      </Route>
+      <Route exact path='/my-profile/'>
+        <MyProfile />
       </Route>
     </div>
   );
--- client/src/services/apiUsers.js
+++ client/src/services/apiUsers.js
@@ -1,34 +1,43 @@
-import api from './apiConfig'
+import api from './apiConfig';
 
-export const signin = async (userData) => {
+export const signin = async userData => {
   try {
-    const response = await api.post(`/signin`, userData)
-    
-    await localStorage.setItem('token', response.data.token)
-    return response.data
-  } catch (error) {
-      throw error
-  }
-}
+    const response = await api.post(`/signin`, userData);
 
-export const signup = async (userData) => {
+    await localStorage.setItem('token', response.data.token);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signup = async userData => {
   try {
-    const response = await api.post(`/signup`, userData)
+    const response = await api.post(`/signup`, userData);
 
-    await localStorage.setItem('token', response.data.token)
-    return response.data
+    await localStorage.setItem('token', response.data.token);
+    return response.data;
   } catch (error) {
-      throw error
+    throw error;
   }
-}
+};
 
 // verifyuser happens with the jwt
 // so no login info required
 export const verifyuser = async () => {
   try {
-    const response = await api.get(`/verifyUser`) 
-    return response.data
+    const response = await api.get(`/verifyUser`);
+    return response.data;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
+
+export const getUser = async id => {
+  try {
+    const response = await api.get(`/users/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
