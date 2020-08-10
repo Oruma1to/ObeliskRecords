88704e0a1bf22e45a8e45be70bff6a6058acf21f
Juan Delgado <Delgado-juan@hotmail.com>
2020-08-09 21:12:38 -0400

    Added the services for editUser ... to udate the user

--- client/src/services/apiUsers.js
+++ client/src/services/apiUsers.js
@@ -41,3 +41,12 @@ export const getUser = async id => {
     throw error;
   }
 };
+
+export const editUser = async id => {
+  try {
+    const response = await api.get(`/users/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
