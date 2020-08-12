06189c3708c1a185200a2a4ac1f76bc9b8d8724a
Juan Delgado <Delgado-juan@hotmail.com>
2020-08-11 22:52:01 -0400

    add conditionals in the app.js foro the /signin rout that only not sign in user can see this page

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,48 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { verifyuser } from './services/apiUsers';
-import { logIn, replaceCart } from './actions';
-import { getCart } from './services/apiCart';
+import React, {useEffect} from 'react'
+import './App.css'
+import {Route, Redirect} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {verifyuser} from './services/apiUsers'
+import {logIn, replaceCart} from './actions'
+import {getCart} from './services/apiCart'
 
-import Signin from './components/users/Signin';
-import Signup from './components/users/Signup';
-import Albums from './components/albums/Albums';
+import Signin from './components/users/Signin'
+import Signup from './components/users/Signup'
+import Albums from './components/albums/Albums'
 
-import Header from './components/shared/Header';
-import Home from './components/shared/Home';
-import Album from './components/albums/Album';
-import CreateAlbum from './components/albums/CRUD/CreateAlbum';
-import EditAlbum from './components/albums/CRUD/EditAlbum';
-import MyProfile from './components/users/MyProfile';
-import EditMyProfile from './components/users/EditMyProfile';
+import Header from './components/shared/Header'
+import Home from './components/shared/Home'
+import Album from './components/albums/Album'
+import CreateAlbum from './components/albums/CRUD/CreateAlbum'
+import EditAlbum from './components/albums/CRUD/EditAlbum'
+import MyProfile from './components/users/MyProfile'
+import EditMyProfile from './components/users/EditMyProfile'
 
-import ShoppingCart from './components/shoppingCart/ShoppingCart';
-
+import ShoppingCart from './components/shoppingCart/ShoppingCart'
 
 function App() {
-  // const isLogged = useSelector(state => state.isLogged)
-  const dispatch = useDispatch();
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
 
   // on component did mount, try to log in and set the user
 
   useEffect(() => {
-    initialize();
-  }, []);
+    initialize()
+  }, [])
 
   const initialize = async () => {
     try {
-      const response = await verifyuser();
+      const response = await verifyuser()
       const data = await getCart()
-      console.log(data)
+      // console.log(data);
       // console.log(response);
-      dispatch(logIn(response));
+      dispatch(logIn(response))
       dispatch(replaceCart(data))
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
+  console.log(isLogged)
 
   return (
     <div className='App'>
@@ -55,10 +55,9 @@ function App() {
         <Home />
       </Route>
       <Route path='/albums/:id'>
-        <Album />
       </Route>
       <Route path='/signin'>
-        <Signin />
+        {isLogged === null ? <Signin /> : <Redirect to='/' />}
       </Route>
       <Route path='/signup'>
         <Signup />
@@ -82,7 +81,7 @@ function App() {
         <EditMyProfile />
       </Route>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
