24560a6629dfa6c1af73712b62316900b6019859
Juan Delgado <Delgado-juan@hotmail.com>
2020-08-12 18:36:25 -0400

    finish create album only for admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,91 @@
-import React, {useEffect} from 'react'
-import './App.css'
-import {Route, Redirect} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {verifyuser} from './services/apiUsers'
-import {logIn, replaceCart} from './actions'
-import {getCart} from './services/apiCart'
+import React, {useEffect} from 'react';
+import './App.css';
+import {Route, Redirect} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {verifyuser} from './services/apiUsers';
+import {logIn, replaceCart} from './actions';
+import {getCart} from './services/apiCart';
 
-import Signin from './components/users/Signin'
-import Signup from './components/users/Signup'
-import Albums from './components/albums/Albums'
+import Signin from './components/users/Signin';
+import Signup from './components/users/Signup';
+import Albums from './components/albums/Albums';
 
-import Header from './components/shared/Header'
-import Home from './components/shared/Home'
-import Album from './components/albums/Album'
-import CreateAlbum from './components/albums/CRUD/CreateAlbum'
-import EditAlbum from './components/albums/CRUD/EditAlbum'
-import MyProfile from './components/users/MyProfile'
-import EditMyProfile from './components/users/EditMyProfile'
+import Header from './components/shared/Header';
+import Home from './components/shared/Home';
+import Album from './components/albums/Album';
+import CreateAlbum from './components/albums/CRUD/CreateAlbum';
+import EditAlbum from './components/albums/CRUD/EditAlbum';
+import MyProfile from './components/users/MyProfile';
+import EditMyProfile from './components/users/EditMyProfile';
 
-import ShoppingCart from './components/shoppingCart/ShoppingCart'
+import ShoppingCart from './components/shoppingCart/ShoppingCart';
 
 function App() {
-  const isLogged = useSelector(state => state.isLogged)
-  const dispatch = useDispatch()
+    const isLogged = useSelector((state) => state.isLogged);
+    const dispatch = useDispatch();
 
-  // on component did mount, try to log in and set the user
+    // on component did mount, try to log in and set the user
 
-  useEffect(() => {
-    initialize()
-  }, [])
+    useEffect(() => {
+        initialize();
+    }, []);
 
-  const initialize = async () => {
-    try {
-      const response = await verifyuser()
-      const data = await getCart()
-      // console.log(data);
-      // console.log(response);
-      dispatch(logIn(response))
-      dispatch(replaceCart(data))
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  console.log(isLogged)
+    const initialize = async () => {
+        try {
+            const response = await verifyuser();
+            const data = await getCart();
+            // console.log(data);
+            // console.log(response);
+            dispatch(logIn(response));
+            dispatch(replaceCart(data));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    console.log(isLogged);
+    console.log(isLogged !== null);
 
-  return (
-    <div className='App'>
-      <Header />
-      <div
-        style={{
-          height: '100px',
-        }}
-      ></div>
-      <Route path='/' exact>
-        <Home />
-      </Route>
-      <Route path='/albums/:id'>
-        <Album />
-      </Route>
-      <Route path='/signin'>{isLogged === null ? <Signin /> : <Redirect to='/' />}</Route>
-      <Route path='/signup'>
-        {isLogged === null ? <Signup /> : <Redirect to='/' />}
-      </Route>
-      <Route exact path='/albums'>
-        <Albums />
-      </Route>
-      <Route path='/createalbum'>
-        <CreateAlbum />
-      </Route>
-      <Route path='/editalbum/:id'>
-        <EditAlbum />
-      </Route>
-      <Route exact path='/cart/'>
-        <ShoppingCart />
-      </Route>
-      <Route exact path='/my-profile/'>
-        <MyProfile />
-      </Route>
-      <Route exact path='/edit-MyProfile/'>
-        <EditMyProfile />
-      </Route>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <Header />
+            <div
+                style={{
+                    height: '100px',
+                }}
+            ></div>
+            <Route path='/' exact>
+                <Home />
+            </Route>
+            <Route path='/albums/:id'>
+                <Album />
+            </Route>
+            <Route path='/signin'>
+                {isLogged === null ? <Signin /> : <Redirect to='/' />}
+            </Route>
+            <Route path='/signup'>
+                {isLogged === null ? <Signup /> : <Redirect to='/' />}
+            </Route>
+            <Route exact path='/albums'>
+                <Albums />
+            </Route>
+            <Route path='/createalbum'>
+                {/* {isLogged !== null ? <CreateAlbum /> : <Redirect to='/' />} */}
+                <CreateAlbum />
+            </Route>
+            <Route path='/editalbum/:id'>
+                <EditAlbum />
+            </Route>
+            <Route exact path='/cart/'>
+                <ShoppingCart />
+            </Route>
+            <Route exact path='/my-profile/'>
+                <MyProfile />
+            </Route>
+            <Route exact path='/edit-MyProfile/'>
+                <EditMyProfile />
+            </Route>
+        </div>
+    );
 }
 
-export default App
+export default App;
